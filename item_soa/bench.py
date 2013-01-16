0c18d5c52aea477efdc805644ed2a603ef3d04cf
Leonardo Vidarte <lvidarte@gmail.com>
2013-01-16 11:24:42 -0300

    Timer way was changed in bench

--- item_soa/bench.py
+++ item_soa/bench.py
@@ -45,113 +45,89 @@ ENVS = {
 
 class Counter(object):
 
-    def __init__(self, name, workers=1):
-        self.workers = workers
+    def __init__(self, name):
         self.name = name
-        self.total_items = 0
-        self.total_time = 0
-        self.max_time = 0
-        self.min_time = 99999999999999999
+        self.items = 0
+        self.time = 0
+        self.max = 0
+        self.min = 99999999999999999
 
     def count_item(self, t):
-        self.total_items += 1
-        self.total_time += t
-        if t > self.max_time:
-            self.max_time = t
-        if t < self.min_time:
-            self.min_time = t
+        self.items += 1
+        self.time += t
+        if t > self.max:
+            self.max = t
+        if t < self.min:
+            self.min = t
 
     def count_counter(self, counter):
-        self.total_items += counter.total_items
-        self.total_time += counter.total_time
-        if counter.max_time > self.max_time:
-            self.max_time = counter.max_time
-        if counter.min_time < self.min_time:
-            self.min_time = counter.min_time
+        self.items += counter.items
+        self.time += counter.time
+        if counter.max > self.max:
+            self.max = counter.max
+        if counter.min < self.min:
+            self.min = counter.min
 
-    def stats(self):
-        if self.total_items > 0:
-            avg_time = self.total_time / float(self.total_items)
-            avg_items = float(self.total_items) / (self.total_time / 1000.0)
+    @property
+    def avg(self):
+        if self.items > 0:
+            return self.time / float(self.items)
         else:
-            avg_time = 0
-            avg_items = 0
-
-        workers = float(self.workers)
-        return {'name': self.name,
-                'items': self.total_items,
-                'time': self.total_time / workers,
-                'time_avg': avg_time / workers,
-                'time_max': self.max_time / workers,
-                'time_min': self.min_time / workers,
-                'items_avg': avg_items * workers,
-                }
+            return 0
 
     def __str__(self):
-        stats = self.stats()
-        fmt = {'name': '%s',
-               'items': '\tItems: %s',
-               'time': '\tTime: %0.2f ms',
-               'time_avg': '\tTime avg: %0.2f ms',
-               'time_max': '\tTime max: %0.2f ms',
-               'time_min': '\tTime min: %0.2f ms',
-               'items_avg': '\tItems Avg: %0.2f/s',
-               }
-        keys = ('name', 'items', 'items_avg', 'time', 'time_avg',
-                'time_max', 'time_min',)
-        return '\n'.join(fmt[k] % stats[k] for k in keys)
+        chunks = (self.name,
+                  'Items: %s' % self.items,
+                  'Time %0.2f ms' % self.time,
+                  'Avg: %0.2f ms' % self.avg,
+                  'Max: %0.2f ms' % self.max,
+                  'Min: %0.2f ms' % self.min,
+                  )
+        return ' - '.join(chunks)
 
 
-def fetch(url, counter, verbose=False):
-    if verbose:
-        sys.stdout.write('.')
-        sys.stdout.flush()
+def fetch(url, reqs_counter):
     begin_time = time.time()
     response = requests.get(url)
-    delta = (time.time() - begin_time) * 1000  # ms
-    counter.count_item(delta)
+    req_time = (time.time() - begin_time) * 1000  # ms
+    reqs_counter.count_item(req_time)
     return response
 
 
-def fetch_subresource(name, url, counter, verbose=False):
-    response = fetch(url, counter, verbose)
+def fetch_subresource(name, url, reqs_counter):
+    response = fetch(url, reqs_counter)
     if response.status_code == 200 and name in ('category', 'location', 'seo'):
         data = response.json()
         if name in ('category', 'location'):
             url_parent = data['response']['resources']['parent']
             if url_parent:
-                fetch_subresource(name, url_parent, counter, verbose)
+                fetch_subresource(name, url_parent, reqs_counter)
         if name == 'seo':
             for direction in ('next', 'prev'):
                 url_seo = data['response']['resources'][direction]
                 if url_seo:
-                    fetch(url_seo, counter, verbose)
+                    fetch(url_seo, reqs_counter)
 
 
-def worker(id, queue, counters, verbose=False):
-    item_counter, reqs_counter = counters
-    if verbose:
-        sys.stdout.write("Worker %s started\n" % id)
-        sys.stdout.flush()
-    time.sleep(2)
-
+def worker(id, queue, counters):
+    items_counter, reqs_counter = counters
     while True:
         try:
             url = queue.get_nowait()
         except Empty:
             break
-        item_reqs_counter = Counter('Item Request')
-        response = fetch(url, item_reqs_counter)
+        begin_time = time.time()
+        response = fetch(url, reqs_counter)
         if response.status_code == 200:
             data = response.json()
             for name, url in data['response']['resources'].items():
                 if url:
-                    fetch_subresource(name, url, item_reqs_counter, verbose)
-        reqs_counter.count_counter(item_reqs_counter)
-        item_counter.count_item(item_reqs_counter.total_time)
+                    fetch_subresource(name, url, reqs_counter)
+        item_time = (time.time() - begin_time) * 1000  # ms
+        items_counter.count_item(item_time)
 
 
-def bench(workers, env, items, verbose=False):
+def bench(workers, env, items):
     # Randomize IDs and construct the urls
     random.shuffle(ids.ids)
     urls_base = itertools.cycle(ENVS[env]['urls_base'])
@@ -160,39 +136,60 @@ def bench(workers, env, items, verbose=False):
         url = '%s/v1/site/1/item/%s' % (urls_base.next(), id)
         queue.put(url)
 
-    # Start threads
+    # Create threads and counters
     jobs = []
     counters = []
     for id in range(workers):
-        c = ([Counter('Item Pages'), Counter('Total Requests')])
-        t = threading.Thread(target=worker, args=(id, queue, c, verbose))
+        items_counter = Counter('Items')
+        reqs_counter = Counter('Requests')
+        c = (items_counter, reqs_counter)
+        t = threading.Thread(target=worker, args=(id, queue, c))
         jobs.append(t)
         counters.append(c)
+
+    # Start timer
+    begin_time = time.time()
+
+    # Start threads
+    for t in jobs:
         t.start()
 
-    # End threads
+    # Wait for end all threads
     for t in jobs:
         t.join()
 
+    # Stop timer
+    bench_time = (time.time() - begin_time) * 1000  # ms
+
     # Show result
-    if verbose:
-        print
-    print_stats(counters, workers)
+    print_stats(bench_time, counters, workers)
 
 
-def print_stats(counters, workers):
-    final_item_counter = Counter('Item Pages', workers)
-    final_reqs_counter = Counter('Total Requests', workers)
+def print_stats(bench_time, counters, workers):
+    items_counter = Counter('Items')
+    reqs_counter = Counter('Requests')
 
     for c in counters:
-        final_item_counter.count_counter(c[0])
-        final_reqs_counter.count_counter(c[1])
+        items_counter.count_counter(c[0])
+        reqs_counter.count_counter(c[1])
 
-    print "-" * 40
-    print workers, "Workers"
-    print "-" * 40
-    print final_item_counter
-    print final_reqs_counter
+    print "=" * 40
+    print workers, "Threads"
+    print "\tTime: %0.2f s" % (bench_time / 1000.0)
+    print "\tTotal Items:", items_counter.items
+    item_avg = float(items_counter.items) / (bench_time / 1000.0)
+    print "\tItems per second: %0.2f/s" % item_avg
+    print "\tTotal requests:", reqs_counter.items
+    req_avg = float(reqs_counter.items) / (bench_time / 1000.0)
+    print "\tRequests per second: %0.2f/s" % req_avg
+    reqs_per_item = (float(reqs_counter.items) / float(items_counter.items))
+    print "\tRequests per item: %0.2f" % reqs_per_item
+    print "\tMax item time: %0.2f ms" % items_counter.max
+    print "\tMin item time: %0.2f ms" % items_counter.min
+    print "\tAvg item time: %0.2f ms" % items_counter.avg
+    print "\tMax request time: %0.2f ms" % reqs_counter.max
+    print "\tMin request time: %0.2f ms" % reqs_counter.min
+    print "\tAvg request time: %0.2f ms" % reqs_counter.avg
 
 
 if __name__ == '__main__':
@@ -206,7 +203,6 @@ if __name__ == '__main__':
                         help='Threads. Can be used multiple times.')
     parser.add_argument('--sleep', type=int, default=10,
                         help='Sleep time in seconds between benchmarks.')
-    parser.add_argument('--verbose', action='store_true')
     args = parser.parse_args()
 
     # Load module with IDs
@@ -222,6 +218,6 @@ if __name__ == '__main__':
 
     # Run benchmarks
     for workers in args.workers:
-        bench(workers, args.env, args.items, args.verbose)
+        bench(workers, args.env, args.items)
         if workers != args.workers[-1]:
             time.sleep(args.sleep)
